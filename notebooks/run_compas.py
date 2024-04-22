e08b885b3fef3b73ea141bf866d888f86b7ddd39
Reinhold Willcox <reinhold.willcox@gmail.com>
2024-04-22 13:41:55 +0200

    replaced path to COMPAS exe

--- notebooks/run_compas.py
+++ notebooks/run_compas.py
@@ -14,21 +14,20 @@
 # ---
 
 import numpy as np
+import h5py as h5
 
 
+# +
 def generate_outdir(str_length=10):
     # Need a random string to hold the data
-
     np.random.seed(np.datetime64('now').astype(int))
     ascii_lowercase = list('abcdefghijklmnopqrstuvwxyz')
     random_str = ''.join(np.random.choice(ascii_lowercase) for i in range(str_length))
     return random_str
-
-
-# +
+    
 def run_compas(*args, **kwargs):
     outdir=generate_outdir()
-    # !./COMPAS -n 1 -o 'data/on_the_fly_data/' -c {outdir}
+    # !././scripts/COMPAS -n 1 -o 'data/on_the_fly_data/' -c {outdir}
     return outdir
     
 def get_myf(outdir):
