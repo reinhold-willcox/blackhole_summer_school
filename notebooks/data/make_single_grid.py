6979ea2fc53da5716939cdb47560c7a2540371be
Reinhold Willcox <reinhold.willcox@gmail.com>
2024-08-02 17:08:03 +0200

    add grid making

--- notebooks/data/make_single_grid.py
+++ notebooks/data/make_single_grid.py
@@ -5,5 +5,5 @@ with open('single_grid_coarse.txt', 'w') as f:
         f.write("--initial-mass-1 {} --initial-mass-2 0.5 -a 10000\n".format(m1))
 
 with open('single_grid_fine.txt', 'w') as f:
-    for m1 in np.logspace(-1, 2, 250):
+    for m1 in np.logspace(np.log10(0.5), 2, 250):
         f.write("--initial-mass-1 {} --initial-mass-2 0.5 -a 10000\n".format(m1))
